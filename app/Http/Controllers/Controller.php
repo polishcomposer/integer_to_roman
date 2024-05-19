16fee93cb5ed2160bc59296f91173ec8fff79439
Dariusz Swoszowski <polishcomposer@gmail.com>
2024-05-19 23:37:38 +0100

    Added frontend for interaction with methods

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Http\Request;
+use App\Services\Converter;
+
+class Controller
 {
-    //
+    public function convertInt(Request $request)
+    {
+        return response()->json(['result' => Converter::integerToRoman(intval($request->input('int-to-roman')))]);
+    }
+
+    public function convertRoman(Request $request)
+    {
+        return response()->json(['result' => Converter::romanToInteger($request->input('roman-to-int'))]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Controller;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/convert-int', [Controller::class, 'convertInt']);
+Route::post('/convert-roman', [Controller::class, 'convertRoman']);
